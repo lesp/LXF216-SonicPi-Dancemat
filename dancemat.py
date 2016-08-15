b9b83cc70ae2a591d457a928759f748e60713842
Les Pounder <lespounder@gmail.com>
2016-08-15 12:41:38 +0000

    Added threading and now we can have a beat

--- dancemat.py
+++ dancemat.py
@@ -1,6 +1,13 @@
 from inputs import get_gamepad
 from psonic import *
+from threading import Thread
 import time
+
+def beat():
+	while True:
+		sample(DRUM_HEAVY_KICK)
+		sleep(0.5)
+
 
 while True:
     events = get_gamepad()
@@ -11,38 +18,40 @@ while True:
         #time.sleep(0.3)
         if button == "BTN_BASE3" and event.state == 1:
             print("SELECT")
-            time.sleep(0.1)
+            #time.sleep(0.1)
         elif button == "BTN_BASE4" and event.state == 1:
         	print("START")
-        	time.sleep(0.1)
+        	beat_thread = Thread(target=beat)
+        	beat_thread.start()
+        	#time.sleep(0.1)
         elif button == "BTN_BASE" and event.state == 1:
         	print("X")
         	sample(LOOP_AMEN, amp=0.5)
-        	sleep(0.877)
+        	#sleep(0.877)
         elif button == "BTN_THUMB2" and event.state == 1:
         	print("UP")
         	time.sleep(0.1)
         	use_synth(SAW)
         	play(60)
-        	sleep(0.1)
+        	#sleep(0.1)
         elif button == "BTN_BASE2" and event.state == 1:
         	print("O")
         	timesleep(0.1)
         elif button == "BTN_TRIGGER" and event.state == 1:
         	print("LEFT")
         	play(62)
-        	sleep(0.1)
+        	#sleep(0.1)
         elif button == "BTN_TOP" and event.state == 1:
         	print("RIGHT")
         	play(64)
-        	sleep(0.1)
+        	#sleep(0.1)
         elif button == "BTN_TOP2" and event.state == 1:
         	print("TRIANGLE")
-        	time.sleep(0.1)
+        	#time.sleep(0.1)
         elif button == "BTN_THUMB" and event.state == 1:
         	print("DOWN")
         	play(66)
-        	sleep(0.1)
+        	#sleep(0.1)
         elif button == "BTN_PINKIE" and event.state == 1:
         	print("SQUARE")
-        	time.sleep(0.1)
+        	#time.sleep(0.1)
