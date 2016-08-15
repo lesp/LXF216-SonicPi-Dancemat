ebbe1a41c15f1f02e228d9e6bf09e56f811ef798
Les Pounder <lespounder@gmail.com>
2016-08-15 16:47:02 +0000

    Updated and final

--- dancemat.py
+++ dancemat.py
@@ -12,44 +12,46 @@ def Amen():
 		sample(LOOP_AMEN, amp=0.5)
 		sleep(1.753)
 
+try:
+	while True:
+	    events = get_gamepad()
+	    for event in events:
+	        print(event.code, event.state)
+	        button = event.code
+	        print("The button pressed was ",button)
+	        use_synth(PROPHET)
+	        if button == "BTN_BASE3" and event.state == 1:
+	            print("SELECT")
+	            Amen_thread = Thread(target=Amen)
+	            Amen_thread.start()   
+	        elif button == "BTN_BASE4" and event.state == 1:
+	        	print("START")
+	        	beat_thread = Thread(target=beat)
+	        	beat_thread.start()
+	        elif button == "BTN_BASE" and event.state == 1:
+	        	print("X")
+	        	sample(DRUM_CYMBAL_CLOSED, amp=0.5)
+	        elif button == "BTN_THUMB2" and event.state == 1:
+	        	print("UP")
+	        	play(60)
+	        elif button == "BTN_BASE2" and event.state == 1:
+	        	print("O")
+	        	sample(DRUM_SNARE_SOFT, amp=0.5)
+	        elif button == "BTN_TRIGGER" and event.state == 1:
+	        	print("LEFT")
+	        	play(62)
+	        elif button == "BTN_TOP" and event.state == 1:
+	        	print("RIGHT")
+	        	play(64)
+	        elif button == "BTN_TOP2" and event.state == 1:
+	        	print("TRIANGLE")
+	        	sample(DRUM_SNARE_HARD, amp=0.5)
+	        elif button == "BTN_THUMB" and event.state == 1:
+	        	print("DOWN")
+	        	play(66)
+	        elif button == "BTN_PINKIE" and event.state == 1:
+	        	print("SQUARE")
+	        	sample(DRUM_BASS_HARD, amp=0.5)
 
-while True:
-    events = get_gamepad()
-    for event in events:
-        print(event.code, event.state)
-        button = event.code
-        print("The button pressed was ",button)
-        use_synth(PROPHET)
-        if button == "BTN_BASE3" and event.state == 1:
-            print("SELECT")
-            Amen_thread = Thread(target=Amen)
-            Amen_thread.start()   
-        elif button == "BTN_BASE4" and event.state == 1:
-        	print("START")
-        	beat_thread = Thread(target=beat)
-        	beat_thread.start()
-        elif button == "BTN_BASE" and event.state == 1:
-        	print("X")
-        	sample(DRUM_CYMBAL_CLOSED, amp=0.5)
-        elif button == "BTN_THUMB2" and event.state == 1:
-        	print("UP")
-        	play(60)
-        elif button == "BTN_BASE2" and event.state == 1:
-        	print("O")
-        	sample(DRUM_SNARE_SOFT, amp=0.5)
-        elif button == "BTN_TRIGGER" and event.state == 1:
-        	print("LEFT")
-        	play(62)
-        elif button == "BTN_TOP" and event.state == 1:
-        	print("RIGHT")
-        	play(64)
-        elif button == "BTN_TOP2" and event.state == 1:
-        	print("TRIANGLE")
-        	sample(DRUM_SNARE_HARD, amp=0.5)
-        elif button == "BTN_THUMB" and event.state == 1:
-        	print("DOWN")
-        	play(66)
-        elif button == "BTN_PINKIE" and event.state == 1:
-        	print("SQUARE")
-        	sample(DRUM_BASS_HARD, amp=0.5)
-
+except KeyboardInterrupt:
+	print("EXIT")
